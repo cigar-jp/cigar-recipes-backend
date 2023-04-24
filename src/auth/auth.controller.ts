62aca5cc89ae26b596f82414306906a6528d66e0
cigar-jp <ryo.shiga.banana@gmail.com>
2023-04-24 13:12:44 +0900

    feat: recipe crud

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,12 +6,11 @@ import {
   HttpStatus,
   Res,
   Req,
-  Get,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
-import { Csrf, Msg } from './interfaces/auth.interface';
+import { Msg } from './interfaces/auth.interface';
 
 @Controller('auth')
 export class AuthController {
